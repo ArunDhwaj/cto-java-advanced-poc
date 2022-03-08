a49157277d0ff34b1026c1aa73b2045e4c9ac94d
ArunDhwaj <sbp.arun.01@gmail.com>
2022-03-08 13:58:46 +0530

    Printing all combination of chunk increasing decreasing by four different threads

--- src/main/java/src/threadssynchronization/PrintRemainder.java
+++ src/main/java/src/threadssynchronization/PrintRemainder.java
@@ -7,10 +7,15 @@ public class PrintRemainder {
 //        System.out.println("1: PrintRemainder");
         PrinterScale print = new PrinterScale();
 
-        Thread t1 = new Thread(new TaskRemainder(print, 40, 1), "Remainder1");
-        Thread t2 = new Thread(new TaskRemainder(print, 40, 2), "Remainder2");
-        Thread t3 = new Thread(new TaskRemainder(print, 40, 3), "Remainder3");
-        Thread t4 = new Thread(new TaskRemainder(print, 40, 4), "Remainder0");
+        Thread t1 = new Thread(new TaskRemainder(print, 40, 1), "Thread1");
+        Thread t2 = new Thread(new TaskRemainder(print, 40, 2), "Thread2");
+        Thread t3 = new Thread(new TaskRemainder(print, 40, 3), "Thread3");
+        Thread t4 = new Thread(new TaskRemainder(print, 40, 4), "Thread4");
+
+//        Thread t1 = new Thread(new TaskRemainder(print, 40, 4), "Thread1");
+//        Thread t2 = new Thread(new TaskRemainder(print, 40, 3), "Thread2");
+//        Thread t3 = new Thread(new TaskRemainder(print, 40, 2), "Thread3");
+//        Thread t4 = new Thread(new TaskRemainder(print, 40, 1), "Thread4");
 
 //        System.out.println("2: PrintRemainder");
         t1.start();
--- src/main/java/src/threadssynchronization/TaskRemainder.java
+++ src/main/java/src/threadssynchronization/TaskRemainder.java
@@ -6,8 +6,7 @@ public class TaskRemainder implements Runnable {
     private PrinterScale print;
     private int remainder;
 
-    public TaskRemainder(PrinterScale print, int max, int remainder)
-    {
+    public TaskRemainder(PrinterScale print, int max, int remainder) {
 //        System.out.println("1. TaskRemainder");
         this.print = print;
         this.max = max;
@@ -20,44 +19,43 @@ public class TaskRemainder implements Runnable {
         int num = remainder;
 
         boolean rev = true;
-        if(!rev) {
-            while (num <= max) {
-                if (remainder == 1) {
-                    print.printRemainder1(num);
-                } else if (remainder == 2) {
-                    print.printRemainder2(num);
-                } else if (remainder == 3) {
-                    print.printRemainder3(num);
-                } else if (remainder == 4) {
-                    print.printRemainder4(num);
-                }
 
-                num = num + 4;
+        printIncreasingChunkAndIncreasingNum(num);
+
+        printIncreasingChunkAndDecreasingNum(num);
+
+    }
+
+    private void printIncreasingChunkAndIncreasingNum(int num) {
+        while (num <= max) {
+            if (remainder == 1) {
+                print.printRemainder1(num);
+            } else if (remainder == 2) {
+                print.printRemainder2(num);
+            } else if (remainder == 3) {
+                print.printRemainder3(num);
+            } else if (remainder == 4) {
+                print.printRemainder4(num);
             }
+
+            num = num + 4;
         }
-        else{
-            while (num <= max)
-            {
-                if (remainder == 1)
-                {
-                    print.printRemainder1Rev(num);
-                }
-                else if (remainder == 2)
-                {
-                    print.printRemainder2Rev(num);
-                }
-                else if (remainder == 3)
-                {
-                    print.printRemainder3Rev(num);
-                }
-                else if (remainder == 4)
-                {
-                    print.printRemainder4Rev(num);
-                }
+    }
 
-                num = num + 4;
+    private void printIncreasingChunkAndDecreasingNum(int num) {
+
+        while (num <= max) {
+            if (remainder == 1) {
+                print.printRemainder1Rev(num);
+            } else if (remainder == 2) {
+                print.printRemainder2Rev(num);
+            } else if (remainder == 3) {
+                print.printRemainder3Rev(num);
+            } else if (remainder == 4) {
+                print.printRemainder4Rev(num);
             }
+
+            num = num + 4;
         }
     }
 }
-
